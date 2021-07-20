0d21d94b6c5d589e3bded8cc53b04e0b0f37d16e
hasti <hastikarimi2002@gmail.com>
2021-07-20 16:25:39 +0430

    How it started...

---
+++ src/main/java/ClientThread.java
@@ -0,0 +1,24 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientThread extends Thread {
+    Socket socket;
+    DataInputStream dataInputStream;
+    DataOutputStream dataOutputStream;
+
+    public ClientThread(Socket socket) {
+        try {
+            this.socket = socket;
+            dataInputStream = new DataInputStream(socket.getInputStream());
+            dataOutputStream = new DataOutputStream(socket.getOutputStream());
+        } catch (IOException ignored) {
+        }
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
---
+++ src/main/java/ServerClass.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerClass {
+    public static void main(String[] args) {
+        try {
+            ServerSocket server = new ServerSocket(7776);
+            while (true) {
+                Socket socket = server.accept();
+                new ClientThread(socket);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
